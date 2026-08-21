96c311f724fd7240ead0d587e5031fa426614b7b
TheEverWeird <ever.hutton@outlook.com>
2026-08-21 11:56:13 +0800

    Fixed everything wrong with electron, moved some files

---
+++ forge.config.js
@@ -0,0 +1,48 @@
+module.exports = {
+  packagerConfig: {
+    asar: true,
+    icon: './img/app-icons/icon',
+    name: 'The Owl House Character Web',
+    osxSign: {},
+    ignore: [
+      /^\/\.git($|\/)/,
+      /^\/\.github($|\/)/,
+      /^\/out($|\/)/
+    ]
+  },
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {
+        name: 'The_Owl_House_Character_Web',
+        setupIcon: './img/app-icons/icon.ico'
+      },
+    },
+    {
+      name: '@electron-forge/maker-dmg',
+      config: {
+        name: 'The Owl House Character Web',
+        icon: './img/app-icons/icon.icns',
+      },
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+  ],
+  plugins: [
+    {
+      name: '@electron-forge/plugin-auto-unpack-natives',
+      config: {},
+    },
+  ],
+};
